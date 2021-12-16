5ab2a7864a871262192f72959261fa1cb30220ec
Mirkomil-838 <muqimovmirkomil@gmail.com>
2021-12-16 18:01:43 +0500

    Yanfgi

--- kod/urls.py
+++ kod/urls.py
@@ -16,9 +16,18 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 from users import views as users_views
+from django.contrib.auth import views as auth_views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('blog.urls')),
-    path('register/',users_views.register,name='register'),
+    path('register/',users_views.register, name='register'),
+    path('profile/', users_views.profile, name="profile"),
+    path('login/', users_views.LoginView.as_view(template_name='users/login.html'), name="login"),
+    path('logout/', users_views.LogoutView.as_view(template_name='users/Logout.html'), name="logout"),
 ]
+
+if settings.DEBUG:
+    urlpatterns+= static(settings.Media_URL,document_root=settings.MEDIA_ROOT)
