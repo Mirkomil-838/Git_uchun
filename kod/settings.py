b32844464ab332a3c6161fa4ef66b77d77864f63
Mirkomil-838 <muqimovmirkomil@gmail.com>
2021-12-16 16:31:18 +0500

    new

--- kod/settings.py
+++ kod/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog',
+    'users',
+     'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -126,3 +128,6 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
